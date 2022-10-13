d2976ea2f5ab021afce8ae41a18f1e049144cb5c
abdulazizch <abdulaziz.dev2@gmail.com>
2022-10-13 22:03:16 +0500

    Project Updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,8 +63,8 @@ $(".swap-btn").click(function(){
 $(document).ready(function() {
     const slider = $('#game-cards-crsl').lightSlider({
         autoWidth:true,
-        loop:true,
-        item:3,
+        loop:false,
+        // item:3,
         controls: false,
         // autoWidth: false,
         slideMargin: 40,
@@ -86,20 +86,42 @@ $(document).ready(function() {
         }
     ],
     onSliderLoad: function() {
-        $('#game-cards-crsl').removeClass('cS-hidden');
-        $('.lSPager').prepend("<span class='me-2 mb-2 btn-slider-nav btn-slide-prev text-white'><img src='./assets/images/icons/arrow-slide-left.png' ></span>")
-        $('.lSPager').append("<span class='ms-2 mb-2 btn-slider-nav btn-slide-next text-white'> <img src='./assets/images/icons/arrow-slide-right.png'></span>")
+        setTimeout(()=>{
+            $('#game-cards-crsl').removeClass('cS-hidden');
+            $('.lSPager').prepend("<span class='me-2 mb-2 btn-slider-nav btn-slide-prev text-white'><img src='./assets/images/icons/arrow-slide-left.png' ></span>")
+            $('.lSPager').append("<span class='ms-2 mb-2 btn-slider-nav btn-slide-next text-white'> <img src='./assets/images/icons/arrow-slide-right.png'></span>")
 
-        $(".btn-slide-prev").click(()=>{
-            slider.goToPrevSlide();
-        })
-        $(".btn-slide-next").click(()=>{
-            slider.goToNextSlide();
-        })
+            $(".btn-slide-prev").click(()=>{
+                slider.goToPrevSlide();
+            })
+            $(".btn-slide-next").click(()=>{
+                slider.goToNextSlide();
+            })
+        },200)
     } 
     });  
 });
 
+const sliUp = new IntersectionObserver((el)=>{
+    el.forEach((e) =>{
+        if(e.isIntersecting){
+            e.target.classList.add("slide-up")
+        }
+        else{
+            e.target.classList.remove("slide-up")
+        }
+    })
+})
+const sliDown = new IntersectionObserver((el)=>{
+    el.forEach((e) =>{
+        if(e.isIntersecting){
+            e.target.classList.add("slide-down")
+        }
+        else{
+            e.target.classList.remove("slide-down")
+        }
+    })
+})
 const fadeUp = new IntersectionObserver((el)=>{
     el.forEach((e) =>{
         if(e.isIntersecting){
@@ -151,14 +173,18 @@ const fadeIn = new IntersectionObserver((el)=>{
     })
 })
 
-const fadeInUp = document.querySelectorAll(".fadeInUp")
+const slideUp = document.querySelectorAll(".slideUp")
+const slideDown = document.querySelectorAll(".slideDown")
 const fadeInDown = document.querySelectorAll(".fadeInDown")
+const fadeInUp = document.querySelectorAll(".fadeInUp")
 const fadeInRight = document.querySelectorAll(".fadeInRight")
 const fadeInLeft = document.querySelectorAll(".fadeInLeft")
 const fadein = document.querySelectorAll(".fadeIn")
 
-fadeInUp.forEach((el) => fadeUp.observe(el))
+slideUp.forEach((el) => sliUp.observe(el))
+slideDown.forEach((el) => sliDown.observe(el))
 fadeInDown.forEach((el) => fadeDown.observe(el))
+fadeInUp.forEach((el) => fadeUp.observe(el))
 fadeInRight.forEach((el) => fadeRight.observe(el))
 fadeInLeft.forEach((el) => fadeLeft.observe(el))
 fadein.forEach((el) => fadeIn.observe(el))
